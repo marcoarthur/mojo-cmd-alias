0498e2618601d8e294942b4439b745f43c60e1bb
Marco Arthur <arthurpbs@gmail.com>
2019-12-10 23:08:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mojo::Command::alias - It's new $module
+
+# SYNOPSIS
+
+    use Mojo::Command::alias;
+
+# DESCRIPTION
+
+Mojo::Command::alias is ...
+
+# LICENSE
+
+Copyright (C) Marco Arthur.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Marco Arthur <arthurpbs@gmail.com>
---
+++ lib/Mojo/Command/alias.pm
@@ -0,0 +1,39 @@
+package Mojo::Command::alias;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojo::Command::alias - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojo::Command::alias;
+
+=head1 DESCRIPTION
+
+Mojo::Command::alias is ...
+
+=head1 LICENSE
+
+Copyright (C) Marco Arthur.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Marco Arthur E<lt>arthurpbs@gmail.comE<gt>
+
+=cut
+
